5cbc507862b50d9bffb1daab1262ce9724fa6e2e
DentonWarnock <dentonwarnock@gmail.com>
2019-11-01 10:04:54 -0700

    API data fetched and stored in state.

--- project/src/App.js
+++ project/src/App.js
@@ -2,12 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <p>test</p>
-    </div>
-  );
+const gitAccountName = "DentonWarnock";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: []
+    };
+  }
+
+  
+  componentDidMount() {
+    fetch(`https://api.github.com/users/${gitAccountName}`)
+      .then(res => res.json())
+      .then(res => {
+        console.log("Component did mount: fetch: res: ", res);
+        this.setState({
+          user: res
+        })
+      })
+      .catch(err => {
+        console.log("Component did mount: fetch: err: ", err)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>test</p>
+      </div>
+    );
+  }  
 }
 
 export default App;
